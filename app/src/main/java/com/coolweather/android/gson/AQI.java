a961740cc98939af30b43d198e03eba88b8a305a
Emiyaalu <583910283@qq.com>
2017-05-08 21:13:46 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,49 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Emiya on 2017/5/7.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
